92d5be5c75e64dfa7f8ea1413dd15999f056440b
Dave Chesser <me@dchesser.website>
2023-05-08 13:46:54 -0400

    new: width and height functions
    Note this is NOT strictly the terminal's size, just the size of
    termbox's buffer. They SHOULD be the same size. Not sure if there's a
    way they aren't when termbox is initialized.

--- janbox2.c
+++ janbox2.c
@@ -35,9 +35,33 @@ cfun_tb_shutdown
   return janet_wrap_integer(result);
 }
 
+static Janet
+cfun_tb_width
+(int32_t argc, Janet *argv) {
+  janet_fixarity(argc, 0);
+
+  int result = tb_width();
+  jb2_error_check(result);
+
+  return janet_wrap_integer(result);
+}
+
+static Janet
+cfun_tb_height
+(int32_t argc, Janet *argv) {
+  janet_fixarity(argc, 0);
+
+  int result = tb_height();
+  jb2_error_check(result);
+
+  return janet_wrap_integer(result);
+}
+
 static JanetReg cfuns[] = {
   {"init"    , cfun_tb_init    , "(init)\n\nInitialize Termbox."},
   {"shutdown", cfun_tb_shutdown, "(shutdown)\n\nShut down Termbox."},
+  {"width"   , cfun_tb_width   , "(width)\n\nReturns termbox's buffer width.\nThis should be the same as the terminal's width."},
+  {"height"  , cfun_tb_height  , "(shutdown)\n\nReturn termbox's buffer height.\nThis should be the same as the terminal's height."},
   {NULL, NULL, NULL},
 };
 
