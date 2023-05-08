72aa03a7da85c652f7b9cf325d5f4c7fe22d6562
Dave Chesser <me@dchesser.website>
2023-05-08 13:46:03 -0400

    refactor: catch-all error checking
    That was going to get old fast having to constantly check
    initialization for every function.

--- janbox2.c
+++ janbox2.c
@@ -2,15 +2,24 @@
 #include "termbox2/termbox2.h"
 
 
+void jb2_error_check(int result) {
+  switch (result) {
+  case TB_ERR_INIT_ALREADY:
+    janet_panic("termbox is already initialized");
+    break;
+  case TB_ERR_NOT_INIT:
+    janet_panic("termbox is not initialized");
+    break;
+  }
+}
+
 static Janet
 cfun_tb_init
 (int32_t argc, Janet *argv) {
   janet_fixarity(argc, 0);
 
   int result = tb_init();
-
-  if (result == TB_ERR_INIT_ALREADY)
-    janet_panic("termbox is already initialized");
+  jb2_error_check(result);
 
   return janet_wrap_integer(result);
 }
@@ -21,9 +30,7 @@ cfun_tb_shutdown
   janet_fixarity(argc, 0);
 
   int result = tb_shutdown();
-
-  if (result == TB_ERR_NOT_INIT)
-    janet_panic("termbox is not initialized");
+  jb2_error_check(result);
 
   return janet_wrap_integer(result);
 }
